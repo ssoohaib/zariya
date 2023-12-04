c90fcf3ff583b38da676199da18112c3420d3878
Sohaib <m.soh.khr@gmail.com>
2023-12-04 14:54:10 +0500

    dummy-data-add

--- dummy_data/dummy_data.js
+++ dummy_data/dummy_data.js
@@ -68,4 +68,123 @@ export const NGOS = [
             'Military Charities',
         ]
     },
+    {
+        id:'sd123adasdwer3242342e7u',
+        title:'Akhuwat Foundation',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!.',
+        logo:'https://img.freepik.com/free-vector/people-care-logo-design-template_474888-1892.jpg?w=740&t=st=1701205331~exp=1701205931~hmac=4d4ed67ff16a1cb947de9b1fb8b653829adcc78e9a092aba8b4165830cd2e395',
+        contact:{
+            phone:'+9234567890',
+            city:'Lahore',
+            country:'Pakistan',
+        },
+        images:[
+            'https://images.unsplash.com/photo-1620608929452-ccc05381b0f7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1530490125459-847a6d437825?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ],
+        causes:[
+            'Disabilities',
+            'Elderly',
+            'Disaster Relief',
+            'Homeless',
+            'Military Charities',
+        ]
+    },
+    {
+        id:'sd4567adasdwer3242342e7u',
+        title:'Akhuwat Foundation',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!.',
+        logo:'https://img.freepik.com/free-vector/people-care-logo-design-template_474888-1892.jpg?w=740&t=st=1701205331~exp=1701205931~hmac=4d4ed67ff16a1cb947de9b1fb8b653829adcc78e9a092aba8b4165830cd2e395',
+        contact:{
+            phone:'+9234567890',
+            city:'Lahore',
+            country:'Pakistan',
+        },
+        images:[
+            'https://images.unsplash.com/photo-1473649085228-583485e6e4d7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1530490125459-847a6d437825?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ],
+        causes:[
+            'Disabilities',
+            'Elderly',
+            'Disaster Relief',
+            'Homeless',
+            'Military Charities',
+        ]
+    },
+    {
+        id:'sd2323adasdwer3242342e7u',
+        title:'Akhuwat Foundation',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!.',
+        logo:'https://img.freepik.com/free-vector/people-care-logo-design-template_474888-1892.jpg?w=740&t=st=1701205331~exp=1701205931~hmac=4d4ed67ff16a1cb947de9b1fb8b653829adcc78e9a092aba8b4165830cd2e395',
+        contact:{
+            phone:'+9234567890',
+            city:'Lahore',
+            country:'Pakistan',
+        },
+        images:[
+            'https://images.unsplash.com/photo-1617450365226-9bf28c04e130?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1530490125459-847a6d437825?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ],
+        causes:[
+            'Disabilities',
+            'Elderly',
+            'Disaster Relief',
+            'Homeless',
+            'Military Charities',
+        ]
+    },
+    {
+        id:'sdfgddeadasdwer3242342e7u',
+        title:'Akhuwat Foundation',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!.',
+        logo:'https://img.freepik.com/free-vector/people-care-logo-design-template_474888-1892.jpg?w=740&t=st=1701205331~exp=1701205931~hmac=4d4ed67ff16a1cb947de9b1fb8b653829adcc78e9a092aba8b4165830cd2e395',
+        contact:{
+            phone:'+9234567890',
+            city:'Lahore',
+            country:'Pakistan',
+        },
+        images:[
+            'https://images.unsplash.com/photo-1459183885421-5cc683b8dbba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1530490125459-847a6d437825?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ],
+        causes:[
+            'Disabilities',
+            'Elderly',
+            'Disaster Relief',
+            'Homeless',
+            'Military Charities',
+        ]
+    },
+    {
+        id:'sdasasadasdwer3242342e7u',
+        title:'Akhuwat Foundation',
+        desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda autem ducimus ipsum at, voluptatibus eum repellendus voluptatum, est iure eveniet esse! Unde dicta dolorum adipisci quasi quis odit temporibus. Amet!.',
+        logo:'https://img.freepik.com/free-vector/people-care-logo-design-template_474888-1892.jpg?w=740&t=st=1701205331~exp=1701205931~hmac=4d4ed67ff16a1cb947de9b1fb8b653829adcc78e9a092aba8b4165830cd2e395',
+        contact:{
+            phone:'+9234567890',
+            city:'Lahore',
+            country:'Pakistan',
+        },
+        images:[
+            'https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1530490125459-847a6d437825?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ],
+        causes:[
+            'Disabilities',
+            'Elderly',
+            'Disaster Relief',
+            'Homeless',
+            'Military Charities',
+        ]
+    },
 ]
